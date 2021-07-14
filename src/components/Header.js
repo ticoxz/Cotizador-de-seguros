2bb7c2e9aa39276030874152aa4c2eb758a4daea
ticoxz <ticomiranda4@gmail.com>
2021-07-14 11:38:19 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #ffffff;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
+
+const Header = ({ titulo }) => {
+    return (
+        <ContenedorHeader>
+            <TextHeader>{titulo}</TextHeader>
+        </ContenedorHeader>
+    );
+}
+
+export default Header;
